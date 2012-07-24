2c8fdd347b8431e9524c26afe54ba5f46a44fed1
phoenix <hf888.huang@gmail.com>
2012-07-23 21:30:17 -0700

    add FirstSolution/

---
+++ FirstSolution/FirstSolution/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstSolution
+{
+    public class Product
+    {
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Discontinued
+        {
+            get;
+            set;
+        }
+    }
+}
